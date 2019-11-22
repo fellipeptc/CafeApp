b8d1e12e73324b4e6ce5b4449672ad535146a20f
Fellipe Prates <fellipeptc@hotmail.com>
2019-11-22 16:45:00 -0300

    Segundo Commit

--- app/src/main/java/com/example/cafeapp/MainActivity.java
+++ app/src/main/java/com/example/cafeapp/MainActivity.java
@@ -1,14 +1,165 @@
 package com.example.cafeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.cafeapp.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.automl.FirebaseAutoMLRemoteModel;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceAutoMLImageLabelerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.List;
+
+import io.fotoapparat.Fotoapparat;
+import io.fotoapparat.result.BitmapPhoto;
+import io.fotoapparat.result.PhotoResult;
+import io.fotoapparat.result.WhenDoneListener;
+import io.fotoapparat.view.CameraView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Fotoapparat fotoapparat;
+    private CameraView cameraView;
+    private FirebaseAutoMLRemoteModel remoteModel;
+    private boolean modelIsDownloaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main); //setando o layout que ele instanciar na tela
+        cameraView = binding.camera;
+
+        binding.btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.hasFocus()) {
+                   // v.setBackground();
+                }
+                //Toast.makeText(getApplicationContext(), "TESTANDO ", Toast.LENGTH_LONG).show();
+                takePicture();
+            }
+        }); //evento de click do botão
+
+        verifyPermission();
+
+
+        FirebaseApp.initializeApp(this);
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .build();
+        remoteModel = new FirebaseAutoMLRemoteModel.Builder("Folhas_de_cafe_01").build(); //chave do modelo treinado firebase
+
+        FirebaseModelManager.getInstance().download(remoteModel, conditions)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        modelIsDownloaded = true;
+                    }
+                });
+
     }
+
+
+    private void takePicture() {
+        PhotoResult photo = fotoapparat.takePicture();
+        Log.d("Fotoapparat", photo.toString());
+        photo.toBitmap().whenDone(new WhenDoneListener<BitmapPhoto>() {
+            @Override
+            public void whenDone(BitmapPhoto bitmap) {
+                try {
+                    doImageRecognition(bitmap.bitmap);
+                } catch (FirebaseMLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void verifyPermission() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                fotoapparat = new Fotoapparat(getBaseContext(), cameraView);
+                fotoapparat.start();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(getBaseContext(), "Sem essa permissão não é possível continuar.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */}
+        }).check();
+    }
+
+
+    //chama o serviço de reconhecimento de imagem do firebase ML kit
+    private void doImageRecognition(Bitmap bitmapPhoto) throws FirebaseMLException {
+        Log.d("Foto", bitmapPhoto.toString());
+        if (modelIsDownloaded) {
+            FirebaseApp.initializeApp(this);
+            FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmapPhoto);
+            FirebaseVisionOnDeviceAutoMLImageLabelerOptions labelerOptions =
+                    new FirebaseVisionOnDeviceAutoMLImageLabelerOptions.Builder(remoteModel).build();
+            FirebaseVision.getInstance().getOnDeviceAutoMLImageLabeler(labelerOptions).processImage(firebaseVisionImage)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionImageLabel> it) {
+                                    //cancelSparkles(); animação
+                                    if (it.isEmpty()) {
+                                        Toast.makeText(getApplicationContext(), "MENSAGEM DE ERRO", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        for (FirebaseVisionImageLabel label : it) {
+                                            Log.d("LIST", label.getText());
+                                            if (label.getConfidence() > 0.4) {
+                                                Toast.makeText(getApplicationContext(), "RECONHECEU :" + label.getText(), Toast.LENGTH_LONG).show();
+                                            } else {
+                                                Toast.makeText(getApplicationContext(), "MENSAGEM DE ERRO DE CONFIANÇA", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+
+                                    }
+
+                                }
+                            }
+                    ).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("LIST", e.getMessage());
+                    //cancelSparkles();
+                    Toast.makeText(getApplicationContext(), "MENSAGEM DE ERRO DE MLKIT", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+            //cancelSparkles();
+            Toast.makeText(getApplicationContext(), "MENSAGEM DE ERRO INTERNET", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
